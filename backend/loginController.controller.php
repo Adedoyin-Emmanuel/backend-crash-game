11cc7339af8039baa67049307b324ea9549fe95a
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-15 01:25:59 +0100

    built the login API

--- backend/loginController.controller.php
+++ backend/loginController.controller.php
@@ -26,8 +26,28 @@ final class LoginController extends DatabaseConnector {
         if($this->statement = $this->conn->prepare($this->db_login_query)){
             
             //bind the parameters 
+            $this->statement->bind_param("s",$this->username);
+            $this->statement->execute();
 
-            
+            $this->result = $this->statement->get_result();
+
+            if(!$this->result){
+                return "An error occured".$this->conn->error;
+            }
+
+            if($this->result->num_rows == 1){
+                $this->user_row = $this->result->fetch_assoc();
+                //compare passwords
+
+                if(password_verify($this->password,$this->user_row["password"])){
+                    return true;
+                }else{
+                    return "invalid credentials";
+                }
+            }else{
+                return "user does not exist";
+            }
+
         }
     }
 }
