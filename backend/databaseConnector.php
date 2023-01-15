74fc5ecce79d1626b1dac01c2853826c32ac210b
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-15 01:06:51 +0100

    Added the database controller file

---
+++ backend/databaseConnector.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// database controller file
+
+
+class DatabaseConnector
+{
+    protected $databaseName = "crash_game";
+    protected $databasePassword = "crash_game";
+    protected $serverName = "localhost";
+    protected $userName = "root";
+
+    public function __construct()
+    {
+
+        
+        
+    }
+}
