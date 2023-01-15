442768f0689de3d3d4565ba9eea946dd4d1410a8
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-15 04:44:17 +0100

    Added the signup controller

--- backend/signupController.controller.php
+++ backend/signupController.controller.php
@@ -64,6 +64,62 @@ class SignupController extends DatabaseConnector
            
             $this->hashed_password = password_hash($this->password, PASSWORD_BCRYPT, $this->options);
             $this->legit_username = mysqli_real_escape_string($this->conn,strtolower($this->username));
+            $this->date_joined = date("Y-m-d"); 
+
+            #check if the user already exist in the database we don't want duplicate users
+		  	$this->user_duplicate_query = "SELECT * FROM crash_game WHERE username = ?";
+
+            if($this->statement = $this->conn->prepare($this->user_duplicate_query)){
+                $this->condition = $legit_username;
+                #bind the parameters
+		  		$this->statement->bind_param("s",$this->condition);
+
+		  		$this->statement->execute();
+
+		  		$this->duplicate_result = $this->statement->get_result();
+
+		  		#error checking
+		  		if(!$this->duplicate_result){
+		  			return "an error occured".$this->conn->error;
+
+		  		}
+
+                if($this->duplicate_result->num_rows > 0){
+                    return "username already exist";
+                }else{
+                    $this->user_duplicate_query="SELECT * FROM crash_game WHERE username = ?";
+                    if($this->statement = $this->conn->prepare($this->user_duplicate_query)){
+                        //bind the parameters
+                        $this->statement->bind_param("s",$this->legit_username);
+                        $this->statement->execute();
+                        $this->duplicate_result= $this->statement->get_result();
+
+
+                        if(!$this->duplicate_result){
+                            return "An error occured".$this->conn->error;
+                        }
+
+                        if($this->duplicate_result->num_rows > 0){
+                            return "username already exist";
+                        }else{
+                            //insert the data
+                            $this->insert_query = "INSERT INTO crash_gamers (username,password,date_joined)";
+
+                            if($this->insert_statement = $this->conn->prepare($this->insert_query)){
+                                $this->insert_query->bind("s,s,s",$this->username,$this->password,$this->date_joined);
+                                $this->insert_statement->execute();
+
+                                return "Signup successful please login";
+                            }else{
+                                return "Signup failded, ".$this->conn->error;
+                            }
+                        }
+                    }
+                }
+
+
+            }
+
     }
 }
 }
