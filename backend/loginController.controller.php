c06a906e6f1b0fdb6df42ad7c1cd1e15534302bf
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-15 01:13:57 +0100

    initial commit for the login controller class file

--- backend/loginController.controller.php
+++ backend/loginController.controller.php
@@ -1,3 +1,14 @@
 <?php
 
-// 
+// login controller file
+
+
+require_once("databaseConnector.php");
+
+final class LoginController extends DatabaseConnector {
+    
+    public function __construct()
+    {
+        
+    }
+}
