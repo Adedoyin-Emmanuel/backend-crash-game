778e50c584ae4786d59ad348dfeda52c71ce21d5
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-15 05:28:11 +0100

    Modified the jquery file

--- signup.php
+++ signup.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php require_once("includes/autoload.php")?>
+    <?php require_once "includes/autoload.php"?>
+    <script src="includes/validateSignup.js"></script>
     <title>SignUp To Crash Game</title>
 </head>
 <body class="container-fluid p-3"> 
