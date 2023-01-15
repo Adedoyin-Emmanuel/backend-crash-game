efeec1756e54a104449a870e19f3e4c8fed5fb73
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-15 05:16:13 +0100

    Creating the validatebackend js code

--- index.php
+++ index.php
@@ -8,7 +8,14 @@
     
     <title>Crash Game</title>
 </head>
-<body>
-    <h1>Welcome To A Blockchain crash game </h1>
+<body class="container-fluid my-3">
+    <h3 class="fs-3 text-capitalize text-center my-3">Welcome To A Blockchain crash game </h3>
+
+
+    <div class="my-5 d-flex align-items-center justify-content-center flex-column">
+
+        <button class="btn btn-primary my-3">Place Bet</button>
+    </div>
+
 </body>
 </html>
